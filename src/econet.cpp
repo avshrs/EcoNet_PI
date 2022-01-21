a8c6a0d5ac3fb0a5cf144e6eafad391fa15280c3
avshrs <avshrs@gmail.com>
2022-01-21 11:30:05 +0100

    up

--- src/econet.cpp
+++ src/econet.cpp
@@ -17,10 +17,12 @@ void EcoNet::run()
         rx_buf.buf.clear();
         tx_buf.buf.clear();
         serial.serial_read(rx_buf);
-        if(rx_buf.buf.at(0)==0x68)
-        {
-            std::cout<<date()<< "ramka start: ";
-            print_buffer(rx_buf.buf.data(),rx_buf.buf.size());
+        for(auto const& value: rx_buf.buf){
+            if(value==0x68)
+            {
+                std::cout<<date()<< "ramka start: ";
+                print_buffer(rx_buf.buf.data(),rx_buf.buf.size());
+            }
         }
         for(auto const& value: rx_buf.buf){
             if(value==0x16)
