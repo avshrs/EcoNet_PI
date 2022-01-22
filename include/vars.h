d666af108cc96fc253ae6dc9e6ed3e9dad425aa1
avshrs <avshrs@gmail.com>
2022-01-22 16:42:27 +0100

    up

--- include/vars.h
+++ include/vars.h
@@ -47,6 +47,7 @@ struct payload{
     float  co_temp = 0;        
     float  return_temp = 0;
     float  flame = 0;
+    float  upper_buffer_temp = 0;
     float  outside_temp = 0;        
     float  weather_temp = 0;   
     float  exhoust_temp = 0;    
