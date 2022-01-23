3cc8ca2756f428b3f861c3f3e3b75706bd313200
avshrs <avshrs@gmail.com>
2022-01-23 01:48:05 +0100

    up

--- src/econet.cpp
+++ src/econet.cpp
@@ -42,18 +42,18 @@ void EcoNet::run()
                 if(header.at(4)==ecomax_address && header.at(7)==ecomax_frame)
                 {
                     // print_buffer(message.data(), message.size() );
-                    // analyze_frame_ecomax_920P1(payload);
+                    analyze_frame_ecomax_920P1(payload);
                 }
                 // else if(header.at(4)==econet_address && header.at(7)==econet_frame)
                 else if(header.at(4)==econet_address)
                 {   
                     // std::cout <<date() << "econet: " ;
-                    print_buffer(message.data(), message.size() );
+                    // print_buffer(message.data(), message.size() );
                 }               
                 else if(header.at(4)==ecoster_address && header.at(7)==ecoster_frame)
                 {
                     // std::cout <<date() << "ecoster: " ;
-                    // print_buffer(message.data(), message.size() );
+                    analyze_frame_ecoster(payload);
                 } 
                 // else
                 // {
