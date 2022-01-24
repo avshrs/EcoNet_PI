1b82b0668764fb53eb44019e3e97e3c4b4c62a6d
avshrs <avshrs@gmail.com>
2022-01-24 12:00:42 +0100

    up

--- src/econet.cpp
+++ src/econet.cpp
@@ -50,6 +50,11 @@ void EcoNet::run()
                     // print_buffer(message.data(), message.size());
                     analyze_frame_ecomax_920P1(payload);
                 }
+                else if(header.at(4)==ecomax_address && header.at(7)!=ecomax_frame)
+                {
+                    print_buffer(message.data(), message.size());
+                    //analyze_frame_ecomax_920P1(payload);
+                }
                 // else if(header.at(4)==econet_address) // debug
                 else if(header.at(4)==econet_address && header.at(7)==econet_frame)
 
